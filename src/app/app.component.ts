00ad3705bbbfce23d0008996e9d272280911c709
Cristian Paez <cpaezariza@gmail.com>
2025-03-26 12:09:51 -0500

    Cierre de sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,18 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   constructor(private authService: AuthService) { }
 
-  // ngOnInit() {
-  //   sessionStorage.setItem('active', 'true'); // Marca la página como activa
-  // }
+  ngOnInit() {
+    sessionStorage.setItem('active', 'true'); // Marca la página como activa
+  }
 
-  // ngOnDestroy() {
-  //   sessionStorage.removeItem('active'); // Limpia el indicador al destruir el componente
-  // }
+  ngOnDestroy() {
+    sessionStorage.removeItem('active'); // Limpia el indicador al destruir el componente
+  }
 
-  // // Detectar cuando se cierra o recarga la pestaña
-  // @HostListener('window:beforeunload', ['$event'])
-  // clearTokenOnUnload(event: Event) {
-
-  //   const isPageActive = sessionStorage.getItem('active')
-
-  //   if (isPageActive) {
-  //     sessionStorage.removeItem('active')
-  //   } else {
-  //     this.authService.logout(); // Elimina el token al cerrar o recargar la pestaña
-  //   }
-  // }
+  // Detectar cuando se cierra o recarga la pestaña
+  @HostListener('window:beforeunload', ['$event'])
+  clearTokenOnUnload(event: Event) {
+    console.log("Cerrando pestaña: Eliminando sesión");
+    this.authService.logout();
+  }
 }
